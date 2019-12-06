c652a473102f7c34f72083d61ae9c4fe31312431
llseng <1300904522@qq.com>
2019-12-06 20:26:04 +0800

    新增workerman包

--- common/function.php
+++ common/function.php
@@ -7,48 +7,48 @@ use Monolog\Handler\StreamHandler;
 function trims($str)
 {
 
-	$search = [" ", "  ", "\r", "\n", "\t", "\0"];
+    $search = [" ", "  ", "\r", "\n", "\t", "\0"];
 
-	$replace = "";
-	//
-	return str_replace($search, $replace, $str);
+    $replace = "";
+    //
+    return str_replace($search, $replace, $str);
 
 }
 
 //文件日志
 function socketLog($file = "", $name = "socket")
 {
-	//去除空字符
-	$file = trims($file);
+    //去除空字符
+    $file = trims($file);
 
-	//替换相对目录
-	$file = str_replace(["../", "./", ".log"], "", $file);
+    //替换相对目录
+    $file = str_replace(["../", "./", ".log"], "", $file);
 
-	//无文件名
-	if(empty($file))
-		$file = "log";
+    //无文件名
+    if(empty($file))
+        $file = "log";
 
-	$path = LOGS_PATH . DS . "socket";
+    $path = LOGS_PATH . DS . "socket";
 
-	$file = $path . DS . date("Ymd") . DS . $file . ".log";
+    $file = $path . DS . date("Ymd") . DS . $file . ".log";
 
-	if( !is_writable( dirname($file) ) && !mkdir( dirname($file), 0777, true) )
-	{
-		$file = $path . DS . date("Ymd") . "log";
-	}
-	
-	//静态变量
-	static $Logger = [];
+    if( !is_writable( dirname($file) ) && !mkdir( dirname($file), 0777, true) )
+    {
+        $file = $path . DS . date("Ymd") . "log";
+    }
+    
+    //静态变量
+    static $Logger = [];
 
-	//不在单例列表
-	if( empty( $Logger[$file] ) )
-	{
-		// 创建日志频道 
-		$log = new Logger($name); 
-		$log->pushHandler(new StreamHandler($file));
-		//保存至单例列表
-		$Logger[$file] = $log;
-	}
+    //不在单例列表
+    if( empty( $Logger[$file] ) )
+    {
+        // 创建日志频道 
+        $log = new Logger($name); 
+        $log->pushHandler(new StreamHandler($file));
+        //保存至单例列表
+        $Logger[$file] = $log;
+    }
 
-	return $Logger[$file];
+    return $Logger[$file];
 }
--- socket/test/server.php
+++ socket/test/server.php
@@ -30,95 +30,95 @@ $server = new TcpServer("127.0.0.1", 1997, 1024);
 //日志事件
 $server->on("log", function ($server ,$msg, $error){
 
-	// echo "\r\n---------start--------\r\n";
-	// echo $msg . "\r\n";
-	// var_export($error);
-	// echo "\r\n----------------------\r\n";
-	socketLog("server", getmypid())->info($msg, $error);
+    // echo "\r\n---------start--------\r\n";
+    // echo $msg . "\r\n";
+    // var_export($error);
+    // echo "\r\n----------------------\r\n";
+    socketLog("server", getmypid())->info($msg, $error);
 });
 
 //主事件
 $server->on("select", function ($server){
-	global $splQueue;
+    global $splQueue;
 
-	$num = 100;
+    $num = 100;
 
-	while ( $num ) {
-		
-		if( !count($splQueue) )
-		{
-			break;
-		}
+    while ( $num ) {
+        
+        if( !count($splQueue) )
+        {
+            break;
+        }
 
-		$data = json_decode( $splQueue->dequeue() , true);
+        $data = json_decode( $splQueue->dequeue() , true);
 
-		if( !$data ) continue;
+        if( !$data ) continue;
 
-		$file = isset( $data['file'] ) ? $data['file'] : '';
-		$name = isset( $data['name'] ) ? $data['name'] : 'tcpServer';
-		$msg = isset( $data['msg'] ) ? $data['msg'] : '';
-		$context = isset( $data['context'] ) ? $data['context'] : [];
+        $file = isset( $data['file'] ) ? $data['file'] : '';
+        $name = isset( $data['name'] ) ? $data['name'] : 'tcpServer';
+        $msg = isset( $data['msg'] ) ? $data['msg'] : '';
+        $context = isset( $data['context'] ) ? $data['context'] : [];
 
-		if( socketLog($file, $name)->info($msg, $context) )
-		{
-			echo "Write Log OK.\r\n";
-		}else{
-			echo "Write Log FAIL.\r\n";
-		}
+        if( socketLog($file, $name)->info($msg, $context) )
+        {
+            echo "Write Log OK.\r\n";
+        }else{
+            echo "Write Log FAIL.\r\n";
+        }
 
-		$num--;
-	}
+        $num--;
+    }
 
 });
 
 //客户端发送事件
 $server->on("write", function ($server, $sock, $msg){
-	
-	global $splQueue,$readBuffer;
-	/*
-	//报结束位置
-	$endPos = strpos($msg, "\n");
+    
+    global $splQueue,$readBuffer;
+    /*
+    //报结束位置
+    $endPos = strpos($msg, "\n");
 
-	if( $endPos === false )
-	{
-		$readBuffer[(int)$sock] .= $msg;
+    if( $endPos === false )
+    {
+        $readBuffer[(int)$sock] .= $msg;
 
-	}else{
-		
-		//截取包
-		$data = $readBuffer[(int)$sock] . substr($msg, 0, $endPos);
+    }else{
+        
+        //截取包
+        $data = $readBuffer[(int)$sock] . substr($msg, 0, $endPos);
 
-		$readBuffer[(int)$sock] = substr($msg, $endPos+1);
+        $readBuffer[(int)$sock] = substr($msg, $endPos+1);
 
-		//入队
-		$splQueue->enqueue($data);
+        //入队
+        $splQueue->enqueue($data);
 
-	}*/
+    }*/
 
-	if( !isset($readBuffer[(int)$sock]) ) $readBuffer[(int)$sock] = "";
+    if( !isset($readBuffer[(int)$sock]) ) $readBuffer[(int)$sock] = "";
 
-	$package = $readBuffer[(int)$sock] . $msg;
+    $package = $readBuffer[(int)$sock] . $msg;
 
-	while ( ($endPos = strpos($package, "\n")) !== false ){
-		//截取包
-		$data = trim(substr($package, 0, $endPos));
+    while ( ($endPos = strpos($package, "\n")) !== false ){
+        //截取包
+        $data = trim(substr($package, 0, $endPos));
 
-		$package = substr($package, $endPos+1);
+        $package = substr($package, $endPos+1);
 
-		//入队
-		if( $data ) $splQueue->enqueue($data);
-	}
+        //入队
+        if( $data ) $splQueue->enqueue($data);
+    }
 
-	$readBuffer[(int)$sock] = $package;
+    $readBuffer[(int)$sock] = $package;
 
-	//var_dump($splQueue, $readBuffer);
+    //var_dump($splQueue, $readBuffer);
 
 });
 
 //客户连接断开事件
 $server->on("close",function ($server, $sock)
 {
-	var_dump($sock,"close");
+    var_dump($sock,"close");
 });
 
 //服务启动
